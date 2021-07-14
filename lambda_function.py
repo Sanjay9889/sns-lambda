b3f212637949298479f6689ad339e9b537c19364
Sanjay Maurya <sanjaymaurya9889@gmail.com>
2021-07-14 12:17:24 +0530

    msg change

--- lambda_function.py
+++ lambda_function.py
@@ -3,14 +3,12 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    message = {"foo": "bar"}
     client = boto3.client('sns')
     response = client.publish(
         TargetArn='arn:aws:sns:us-east-1:499490402390:test_topic',
-        Message=json.dumps({'default': json.dumps(message),
-                            'sms': 'here a short version of the message',
-                            'email': 'here a longer version of the message'}),
-        Subject='a short subject for your message',
+        Message=json.dumps({
+            'email': 'Please stay inside home.'}),
+        Subject='Covid-19 Update',
         MessageStructure='json'
     )
 
